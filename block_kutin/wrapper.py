e628c6eef4702002559ab61d64e3b297e6f68cae
Simon Martiel <simon.martiel@gmail.com>
2023-05-04 12:11:32 +0200

    Updated docstrings

--- block_kutin/wrapper.py
+++ block_kutin/wrapper.py
@@ -9,6 +9,11 @@ class Database:
     """
     A class managing database generation, loading, and queries for
     the block Kutin algorithm.
+
+
+    Arguments:
+        topology_name (str): the name of the topology (see the paper or the src/topology.rs file for more details)
+        database_folder (optional, str): the directory in which to store the database files. Default is `~/.database`
     """
 
     def __init__(self, topology_name, database_folder="~/.database"):
@@ -30,6 +35,23 @@ class Database:
         return load_database(fname)
 
     def get_circuit(self, matrix, step=1):
+        """
+        Queries the database to find the shallowest implementation of a given operator.
+
+        The operator is specified by its F_2 matrix:
+
+        - for step 1 operators: the matrix is expected to be full rank and to have shape (2k, k)
+          where k is the block size
+        - for step 2 operators: the matrix is expected to be full rank, to have shape (2k, 2k)
+          where k is the block size, and to have only 0s in its bottom right k x k block.
+
+        Argument:
+            matrix (np.ndarray): a numpy array representing the operator
+            step (optional, int): the step (either 1 or 2, see paper for more details)
+
+        Returns:
+            list: a list of pairs (control, target) describing the CNOT implementation
+        """
         return get_circuit_from_matrix(
             matrix.T.flatten(), self.databases[step == 2], step
         )
